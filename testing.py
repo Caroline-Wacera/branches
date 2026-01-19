6f8b26d32c2e65067f35ecf67351c1d9f8452a8f
Melvinn-k <Kinyuamelvin25@gmail.com>
2026-01-19 09:52:32 +0300

    added lines to my file.

---
+++ testing.py
@@ -0,0 +1,11 @@
+#5️⃣ Count Up
+#Print numbers from 5 to 10.
+for number in range(5, 11):
+    print(number)
+
+
+#6️⃣ Skip a Number
+#Print numbers from 1 to 10, but skip 6.
+for number in range(1, 11):
+    if number != 6:
+        print(number)
